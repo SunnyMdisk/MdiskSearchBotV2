39dfc6f7bb6ce79e400127e19382007bec2f2953
SunnyMdisk <106555973+SunnyMdisk@users.noreply.github.com>
2023-01-15 11:44:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
+git clone https://github.com/Movies4u/Movies4u.git /Movies4u
 cd /MdiskSearchBotV2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
